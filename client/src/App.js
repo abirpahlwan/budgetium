f25bb9caaba97e4213a552ea41203ee3c69e00e6
abirpahlwan <abir978@gmail.com>
2022-11-22 06:57:22 +0600

    add page home

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home></Home>}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
