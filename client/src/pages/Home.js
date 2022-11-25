bafba3c770e389ccaea4069c6107c912005b533b
abirpahlwan <abir978@gmail.com>
2022-11-26 04:44:47 +0600

    fetch expenses

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+    const [expenses, setExpenses] = useState(null);
+
+    useEffect(() => {
+        const fetchExpenses = async () => {
+            const response = await fetch("http://localhost:4000/api/expenses");
+            const json = await response.json();
+
+            if(response.ok) {
+                setExpenses(json.data);
+            }
+        };
+
+        fetchExpenses();
+    }, []);
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="expense-list">
+                {expenses && expenses.map((expense) => (
+                    <p key={expense._id}>{expense.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
