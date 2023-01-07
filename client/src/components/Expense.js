ad95f5f82e5193aa36070676f905dae6c71b10ce
abirpahlwan <abir978@gmail.com>
2023-01-07 20:46:56 +0600

    view expense

---
+++ client/src/components/Expense.js
@@ -0,0 +1,10 @@
+const Expense = ({expense}) => {
+    return (
+        <div className="expense">
+            <p>{expense.title}</p>
+            <p></p>
+        </div>
+    )
+}
+
+export default Expense;
