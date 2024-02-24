3b4139fc398d33d896a59b14a0d42165e2c232bd
abirpahlwan <abir978@gmail.com>
2024-02-24 08:48:58 +0600

    jwt when register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,18 +4,35 @@ const { StatusCodes, ReasonPhrases } = require('http-status-codes');
 
 const User = require("../models/user");
 
+const generateToken = (username, email) => {
+    const payload = {
+        sub: username,
+        email: email,
+        iat: Math.floor(Date.now() / 1000),
+    };
+    return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRE});
+};
+
 const register = async (req, res, next) => {
     try {
         const user = await User.create(req.body);
+        const jwt = generateToken(user.username, user.email);
+        // or
+        // const user = new User(req.body);
         // await user.save();
 
         res.status(200).json({
             success: true,
-            data: user
+            message: "User created",
+            data: {
+                jwt: jwt,
+                user: user
+            }
         });
     } catch (error) {
         res.status(400).json({
             success: false,
+            message: "User not created",
             error: error.message
         });
     }
